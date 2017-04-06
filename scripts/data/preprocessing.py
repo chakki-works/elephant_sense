58b52b3cb47a5dbb9c76f87732d5ff81a3a67570
Hironsan <light.tree.1.13@gmail.com>
2017-04-06 13:48:45 +0900

    implement preprocessor

---
+++ scripts/data/preprocessing.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import re
+
+from scripts.data.tokenizer import MeCabTokenizer
+from scripts.data.normalization import normalize_unicode, normalize_number
+from scripts.data.cleaning import clean_code, clean_html_tags
+DATA_DIR = os.path.join(os.path.dirname(__file__), '../../data/processed')
+
+
+def cleaning(text):
+    replaced_text = text.lower()
+    replaced_text = clean_code(html_text=replaced_text)  # remove source code
+    replaced_text = clean_html_tags(html_text=replaced_text)  # remove html tag
+    replaced_text = re.sub(r'\$.*?\$+', '', replaced_text)  # remove math equation
+    replaced_text = re.sub(r'[@＠]\w+', '', replaced_text)  # remove @mention
+    replaced_text = re.sub(r'https?:\/\/.*?[\r\n ]', '', replaced_text)  # remove URL
+    replaced_text = re.sub(r'　', '', replaced_text)  # remove zenkaku space
+    return replaced_text
+
+
+def normalization(text):
+    normalized_text = normalize_unicode(text)
+    normalized_text = normalize_number(normalized_text)
+    return normalized_text
+
+
+def process(text):
+    tokenizer = MeCabTokenizer('/usr/local/lib/mecab/dic/mecab-ipadic-neologd/')
+    text = cleaning(text)
+    text = normalization(text)
+    words = tokenizer.wakati_baseform(text)
+    return words
+
+
+if __name__ == '__main__':
+    items = {'data': [], 'labels': [], 'files': []}
+    dir_name = os.path.join(DATA_DIR, 'items')
+    for file_name in os.listdir(dir_name):
+        if not file_name.endswith('.json'):
+            continue
+        with open(os.path.join(dir_name, file_name)) as f:
+            item = json.load(f)
+            items['data'].append(item['rendered_body'])
+            items['labels'].append(item['annotations'])
+            items['files'].append(file_name)
+
+    data = [process(text) for text in items['data']]
+    items['data'] = data
+
+    with open(os.path.join(DATA_DIR, 'posts.json'), 'w') as f:
+        json.dump(items, f)
---
+++ scripts/data/tokenizer.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from collections import namedtuple
+
+import MeCab
+
+
+class MeCabTokenizer(object):
+
+    def __init__(self, user_dic_path='', sys_dic_path=''):
+        option = ''
+        if user_dic_path:
+            option += ' -d {0}'.format(user_dic_path)
+        if sys_dic_path:
+            option += ' -u {0}'.format(sys_dic_path)
+        self._t = MeCab.Tagger(option)
+
+    def wakati(self, sent):
+        words = [token.surface for token in self.tokenize(sent)]
+        return words
+
+    def wakati_baseform(self, sent):
+        words = [token.base_form if token.base_form != '*' else token.surface
+                 for token in self.tokenize(sent)]
+        return words
+
+    def tokenize(self, text):
+        self._t.parse('')
+        chunks = self._t.parse(text.rstrip()).splitlines()[:-1]  # Skip EOS
+        token = namedtuple('Token', 'surface, pos, pos_detail1, pos_detail2, pos_detail3,\
+                                                                         infl_type, infl_form, base_form, reading, phonetic')
+        for chunk in chunks:
+            if chunk == '':
+                continue
+            surface, feature = chunk.split('\t')
+            feature = feature.split(',')
+            if len(feature) <= 7:  # 読みがない
+                feature.append('')
+            if len(feature) <= 8:  # 発音がない
+                feature.append('')
+            yield token(surface, *feature)
+
+    def filter_by_pos(self, sent, pos=('名詞',)):
+        tokens = [token for token in self.tokenize(sent) if token.pos == pos]
+        return tokens
