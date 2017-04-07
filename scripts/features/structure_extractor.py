2f2f64d60863acb9254ea4998e3c55059d329dad
kamujun <junya.kamura@gmail.com>
2017-04-07 16:40:05 +0900

    add structure feature & fix save models

--- scripts/features/structure_extractor.py
+++ scripts/features/structure_extractor.py
@@ -11,6 +11,7 @@ class ItemizationCountExtractor(FeatureExtractor):
 
         return count
 
+
 class ImageCountExtractor(FeatureExtractor):
 
     def extract(self, post, extracted=None):
@@ -26,3 +27,43 @@ class FormulaCountExtractor(FeatureExtractor):
         count = len(re.findall(r'\$.*?\$+', post.rendered_body))
         return count
 
+
+class ItemizationRatioExtractor(FeatureExtractor):
+
+    def __init__(self, text):
+        self.text = text
+
+    def extract(self, post, extracted=None):
+        soup = BeautifulSoup(post.rendered_body, "html.parser")
+        target_count = len(soup.find_all("ul"))
+        lines_count = len(self.text.split("。"))
+
+        ratio = target_count / lines_count if target_count != 0 else 0
+        return ratio
+
+
+class ImageRatioExtractor(FeatureExtractor):
+
+    def __init__(self, text):
+        self.text = text
+
+    def extract(self, post, extracted=None):
+        soup = BeautifulSoup(post.rendered_body, "html.parser")
+        target_count = len(soup.find_all("img"))
+        lines_count = len(self.text.split("。"))
+
+        ratio = target_count / lines_count if target_count != 0 else 0
+        return ratio
+
+
+class FormulaRatioExtractor(FeatureExtractor):
+
+    def __init__(self, text):
+        self.text = text
+
+    def extract(self, post, extracted=None):
+        target_count = len(re.findall(r'\$.*?\$+', post.rendered_body))
+        lines_count = len(self.text.split("。"))
+
+        ratio = target_count / lines_count if target_count != 0 else 0
+        return ratio
